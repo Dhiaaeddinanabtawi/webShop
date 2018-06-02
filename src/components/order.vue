c83c6bd1787c10628683e41e60825354870f364b
Dhiaa Eddin Anabtawi <dhiaaeddin.anabtawi@gmail.com>
2018-06-02 14:15:59 +0200

    Changing some standards

--- src/components/order.vue
+++ src/components/order.vue
@@ -1,6 +1,6 @@
 <template>
 	<form id="order" v-bind:class="{showForm:formVisible}">
-        <button id="open-order" v-on:click="showForm">Beställ</button>
+        <button id="open-order" @click.prevent="showForm">Beställ</button>
         <input type="text" id="first-name" placeholder="Förnamn">
         <input type="text" id="last-name" placeholder="Efternamn">
         <input type="tel" id="phone" placeholder="Telefonnr">
@@ -8,7 +8,7 @@
         <input type="text" id="street"  placeholder="Gatuadress">  
         <input type="text" id="zip"  placeholder="Postnr">  
         <input type="text" id="city"  placeholder="Ort">  
-        <input type="submit" value="Skicka" v-on:click="showMessage">
+        <input type="submit" value="Skicka" @click="showMessage">
       </form>
 </template>
 
@@ -22,7 +22,6 @@
         },
         methods: {
             showForm: function(event){
-                event.preventDefault();
                 this.formVisible = ! this.formVisible;
             },
             showMessage: function(){
